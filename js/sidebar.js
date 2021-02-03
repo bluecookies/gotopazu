169dc8ec8411fe1e60cb3d98ceb0fe713db09105
bluecookies <landentire@gmail.com>
2021-02-03 17:55:20 +1100

    fix relative links

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,9 +2,9 @@
 var sidebar = $("<div>", { "class": "sidebar" });
 
 sidebar
-	.append($("<div>").append( $("<a>", { href: "/" }).append("Main") ))
-	.append($("<div>").append( $("<a>", { href: "/cards.html" }).append("Cards") ))
-	.append($("<div>").append( $("<a>", { href: "/homework.html" }).append("Homework") ))
+	.append($("<div>").append( $("<a>", { href: ".." }).append("Main") ))
+	.append($("<div>").append( $("<a>", { href: "cards.html" }).append("Cards") ))
+	.append($("<div>").append( $("<a>", { href: "homework.html" }).append("Homework") ))
 
 $("body").prepend(sidebar);
 
